85163df30e40fe9f16929e42957f201144c1a408
Bur0 <burakalpkara@gmail.com>
2020-11-16 09:59:11 +0300

    Update server.js

--- miragejs/server.js
+++ miragejs/server.js
@@ -18,7 +18,7 @@ export function makeServer({ environment = 'development' } = {}) {
         durationDayText: 'Taze çıktı!',
         imageUrl:
           'http://localhost/bluecollar/Company/Job/000/000/000/avatar/13920170713010155805.jpg',
-        distance: '8,53 km yakınında',
+        distance: '4,2 km yakınında',
         latitude: 41.050968170166023,
         longitude: 29.062856674194339,
         cityName: 'İstanbul',
@@ -26,8 +26,8 @@ export function makeServer({ environment = 'development' } = {}) {
       })
       server.create('joblist', {
         jobId: 'C71CD1954BA149BCAF42B25F775EA4FB2',
-        positionName: 'İngilizce Öğretmeni',
-        companyName: 'Testoğlu Lahmacun Pide Ve Kebap',
+        positionName: 'Diş Doktoru',
+        companyName: 'İmperial Hastanesi',
         durationDay: 0,
         durationDayText: 'Taze çıktı!',
         imageUrl:
@@ -36,19 +36,33 @@ export function makeServer({ environment = 'development' } = {}) {
         latitude: 41.050968170166023,
         longitude: 29.062856674194339,
         cityName: 'Ankara',
-        townName: 'Çengelköy Mahallesi',
+        townName: 'Altındağ Mahallesi',
+      })
+      server.create('joblist', {
+        jobId: 'C71CD1954BA149BCAF42B25F775EA4FB3',
+        positionName: 'Frontend Developer',
+        companyName: 'ASD bilişim',
+        durationDay: 0,
+        durationDayText: 'Taze çıktı!',
+        imageUrl:
+          'http://localhost/bluecollar/Company/Job/000/000/000/avatar/13920170713010155805.jpg',
+        distance: '9 km yakınında',
+        latitude: 41.050968170166023,
+        longitude: 29.062856674194339,
+        cityName: 'İzmir',
+        townName: 'Kiraz Mahallesi',
       })
       server.create('jobdetail', {
         jobId: 'C71CD1954BA149BCAF42B25F775EA4FB1',
-        positionName: 'Jandarma Bölge Komutanı',
-        companyName: 'Vosyoyvsyvosyv',
+        positionName: 'Gerber Operatörü',
+        companyName: 'Testoğlu Lahmacun Pide Ve Kebap',
         durationDay: 0,
         durationDayText: 'Taze çıktı!',
         imageUrl: 'https://localhost/bluecollar/company/job/Default/6.jpg',
         countryCode: 'TR',
         countryName: 'Turkey',
         cityName: 'İstanbul',
-        townName: 'Ümraniye',
+        townName: 'Çengelköy Mahallesi',
         address:
           'Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey',
         postalCode: 34768,
@@ -56,7 +70,8 @@ export function makeServer({ environment = 'development' } = {}) {
         hasLongitude: true,
         latitude: 41.0266502,
         longitude: 29.1157803,
-        description: 'syovsvyosvyoyvsoby',
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
         status: 1,
         contactPhone: {
           countryCallingCode: '+90',
@@ -66,23 +81,49 @@ export function makeServer({ environment = 'development' } = {}) {
       })
       server.create('jobdetail', {
         jobId: 'C71CD1954BA149BCAF42B25F775EA4FB2',
-        positionName: 'Jandarma 2 Bölge Komutanı',
-        companyName: 'Vosyoyvsyvosyv',
+        positionName: 'Diş Doktoru',
+        companyName: 'İmperial Hastanesi',
         durationDay: 0,
         durationDayText: 'Taze çıktı!',
         imageUrl: 'https://localhost/bluecollar/company/job/Default/6.jpg',
         countryCode: 'TR',
         countryName: 'Turkey',
-        cityName: 'İstanbul',
-        townName: 'Ümraniye',
-        address:
-          'Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey',
+        cityName: 'Ankara',
+        townName: 'Altındağ Mahallesi',
+        address: 'test test',
         postalCode: 34768,
         hasLatitude: true,
         hasLongitude: true,
         latitude: 41.0266502,
         longitude: 29.1157803,
-        description: 'syovsvyosvyoyvsoby',
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+        status: 1,
+        contactPhone: {
+          countryCallingCode: '+90',
+          areaCode: '879',
+          number: '9797979',
+        },
+      })
+      server.create('jobdetail', {
+        jobId: 'C71CD1954BA149BCAF42B25F775EA4FB3',
+        positionName: 'Frontend Developer',
+        companyName: 'ASD bilişim',
+        durationDay: 0,
+        durationDayText: 'Taze çıktı!',
+        imageUrl: 'https://localhost/bluecollar/company/job/Default/6.jpg',
+        countryCode: 'TR',
+        countryName: 'Turkey',
+        cityName: 'İzmir',
+        townName: 'Kiraz Mahallesi',
+        address: 'Test Mahallesi Meşe Yolu Sokak No:2 34768 Kiraz/İzmir Turkey',
+        postalCode: 34768,
+        hasLatitude: true,
+        hasLongitude: true,
+        latitude: 41.0266502,
+        longitude: 29.1157803,
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
         status: 1,
         contactPhone: {
           countryCallingCode: '+90',
